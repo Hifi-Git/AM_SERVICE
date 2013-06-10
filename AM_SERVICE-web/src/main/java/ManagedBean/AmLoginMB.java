c415ac54dd4bca1a3f06b459d161c12800266449
a.mereu <rompila@gmail.com>
2013-06-10 17:53:44 +0200

    aggiunto il primo managedBean

---
+++ AM_SERVICE-web/src/main/java/ManagedBean/AmLoginMB.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ManagedBean;
+
+/**
+ *
+ * @author anto
+ */
+public class AmLoginMB {
+
+    /**
+     * Creates a new instance of AmLoginMB
+     */
+    public AmLoginMB() {
+    }
+}
