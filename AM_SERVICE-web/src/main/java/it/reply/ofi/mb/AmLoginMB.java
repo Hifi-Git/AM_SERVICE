d7ca9bf9e9f5981b7dcb2aceb154e7eb5bf723ea
a.mereu <rompila@gmail.com>
2013-06-14 15:40:53 +0200

    Inserito l'ManagedBean di prova e libreria Primefaces

--- AM_SERVICE-web/src/main/java/it/reply/ofi/mb/AmLoginMB.java
+++ AM_SERVICE-web/src/main/java/it/reply/ofi/mb/AmLoginMB.java
@@ -2,17 +2,22 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package ManagedBean;
+package it.reply.ofi.mb;
 
 /**
  *
  * @author anto
  */
+
 public class AmLoginMB {
 
     /**
      * Creates a new instance of AmLoginMB
      */
     public AmLoginMB() {
+    }
+    public String getValue(){
+    
+    return "ok";
     }
 }
